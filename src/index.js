b9f5b70ef1713ef05a2f001afd878b7ae14b922b
Yourdad404 <arshadaamir09@gmail.com>
2023-08-14 08:57:12 +0530

    Added db model for notifications

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express=require('express')
 const app=express();
 const {PORT}=require('./config/serverConfig')
 const bodyParser=require('body-parser')
+const cron=require('node-cron')
+
+
+
 const serverStart=()=>{
     
     app.use(bodyParser.json());
@@ -9,6 +13,9 @@ const serverStart=()=>{
    
     app.listen(PORT,()=>{
         console.log(`Server is listening on Port ${PORT}`)
+        // cron.schedule('*/1 * * * *',()=>{
+        //     console.log("1 minute has passed")
+        // })
     })
 }
 serverStart();
